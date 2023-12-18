e0065245c477e6e5aaeaba1b7408816b14f0f913
barrolskii <ajbtdc@gmail.com>
2023-12-18 18:55:15 +0000

    Deploying to gh-pages from @ barrolskii/barrolskii.github.io@c3492a219c058387d82887f3e84cf867afddb8c9 🚀

---
+++ assets/js/common.js
@@ -0,0 +1,63 @@
+function myOnClick () {
+    var bodyTag = document.getElementsByTagName('body')[0];
+    const sidebarAtrribute = 'sidebar-display';
+
+    if (bodyTag.hasAttribute(sidebarAtrribute)) {
+        bodyTag.removeAttribute(sidebarAtrribute);
+    } else {
+        bodyTag.setAttribute(sidebarAtrribute , '');
+    }
+}
+
+function toggleTheme() {
+  var htmlTag = document.getElementsByTagName('html')[0];
+  const attribute = 'data-mode';
+  if (htmlTag.hasAttribute(attribute)) {
+    htmlTag.removeAttribute(attribute);
+  } else {
+    htmlTag.setAttribute(attribute, 'light');
+  }
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function setDescription() {
+	var descriptionTag = document.getElementsByClassName('site-subtitle')[0];
+
+	if (descriptionTag === null) {
+		console.error('Could not find description tag');
+		return;
+	}
+
+	const descriptions = [
+		'Build Engineer',
+		'I\'m not actually a ghost',
+		'200k+ gamerscore and counting',
+	];
+
+	var value = getRandomInt(descriptions.length);
+
+	descriptionTag.innerText = descriptions[value];
+}
+
+document.addEventListener('scroll', (event) => {
+    var toTopButton = document.getElementById('back-to-top');
+    if (window.scrollY > 50) {
+        toTopButton.style.display = 'inline-block';
+    } else {
+        toTopButton.style.display = 'none';
+    }
+});
+
+
+var toTopButton = document.getElementById('back-to-top');
+toTopButton.addEventListener('click', () => {
+    window.scrollTo({
+        top:0,
+        behavior: 'smooth'
+    });
+});
+
+setDescription();
