85b618216760d6cb4132245f43e8112c95c655ae
barrolskii <ajbtdc@gmail.com>
2024-03-17 20:49:30 +0000

    Deploying to gh-pages from @ barrolskii/barrolskii.github.io@eab42c618d0fac919da7f682519f59d666e660f7 🚀

--- assets/js/canvas.js
+++ assets/js/canvas.js
@@ -1,6 +1,14 @@
+export var canvas = undefined;
+export var ctx = undefined;
+
+function initCanvas() {
+    canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
+}
+
 function updateCanvasSize() {
-    var contentContainer = document.getElementsByClassName("content")[0];
-    var mainContainer = document.getElementsByTagName("main")[0];
+    let contentContainer = document.getElementsByClassName("content")[0];
+    let mainContainer = document.getElementsByTagName("main")[0];
 
     if (contentContainer === null) {
         console.error("Could not find content container when updating canvas size");
@@ -12,11 +20,29 @@ function updateCanvasSize() {
         return;
     }
 
-    canvas.width = ceilInt(contentContainer.offsetWidth);
-    canvas.height = mainContainer.offsetHeight / 3;
+    canvas.width = Math.ceil((contentContainer.offsetWidth / 10)) * 10;
+    canvas.height = mainContainer.offsetHeight;
 }
 
-window.addEventListener("resize", (event) => {
-  updateCanvasSize();
-  draw();
-});
+function setCanvasResizeFunction(callback) {
+      if (typeof callback !== 'function') {
+        console.error("Expected function as argument");
+        return;
+    }
+
+    updateCanvasSize = callback;
+}
+
+function setCanvasResizeEvent(callback) {
+     if (typeof callback !== 'function') {
+        console.error("Expected function as argument");
+        return;
+    }
+
+    window.addEventListener("resize", (event) => {
+        updateCanvasSize();
+        callback();
+    });
+}
+
+export { initCanvas, updateCanvasSize, setCanvasResizeFunction, setCanvasResizeEvent }
