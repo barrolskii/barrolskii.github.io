6baec856f0c3efbe0e7f1cec9d6029a350536509
barrolskii <ajbtdc@gmail.com>
2025-08-01 16:12:36 +0000

    Deploying to gh-pages from @ barrolskii/barrolskii.github.io@96e12f753e649178811753d199525af2c6efbac2 🚀

--- assets/js/modules/time/time.js
+++ assets/js/modules/time/time.js
@@ -1,3 +1,8 @@
+/**
+ * Sleeps for specified number of milliseconds
+ * @param {number} ms The amount of time to sleep for in milliseconds
+ * @return {Promise}
+ */
 function sleep(ms) {
     if (ms === 0) {
         return;
